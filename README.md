75410e2bf814e42b6c8870eefc661ae798c7a564
James Huber <jhuber8@cmn.edu>
2015-07-22 22:08:58 -0500

    second push, with milestone 1 coded in html

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pwp
 james's personal website project
+This is James Huber's personal website project. It will be a portfolio website to showcase James and his newly aquired coding skills to the world.
---
+++ documentation/milestone-1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Personal Website Project</title>
+	</head>
+	<body>
+		<div>
+			<header>
+				<h1>Personal Website Project</h1>
+			</header>
+			<div>
+				<div>
+					<h2>PURPOSE</h2>
+					<p>I would like to create a personal portfolio website which will advertise me and my coding skills to potential clients/employers</p>
+				</div>
+				<div>
+					<h2>AUDIENCE</h2>
+					<p>Potention clients and employers interested in getting a full view of all my web development skills, including past and future projects.</p>
+				</div>
+				<div>
+					<h2>GOAL</h2>
+					<p>The create a fully functional and elegant website which demonstrates my full array of coding skills,
+						in order to attract potential employers or freelance clients. This website will be comprehensive and
+						fully updatable with links to new or current projects and will tell a little about myself	and my philosophy
+						and goals.</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
